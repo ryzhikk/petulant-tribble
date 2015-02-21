ad066a6de03669bee4266ca9cf8049d907659558
ryzhikk <dianakrylowa@yandex.ru>
2015-02-21 21:39:14 +0300

    Так?

--- www/models/class_news.php
+++ www/models/class_news.php
@@ -5,26 +5,26 @@ class News extends Article {
 
     public function __construct() {
         $this->sql_object = new Sql_query();
-        $this->sql_table = new C_config();
-
+        $table = new C_config();
+        $this->sql_table = $table->table_news;
     }
 
     public function Get_all_article() {
 
-        $news = $this->sql_object->Get_all($this->sql_table->table_news);
+        $news = $this->sql_object->Get_all($this->sql_table);
         return $news;
     }
 
     public function Get_one_article() {
 
-        $new = $this->sql_object->Get_one_line($this->sql_table->table_news, $this->id);
+        $new = $this->sql_object->Get_one_line($this->sql_table, $this->id);
         return $new;
     }
 
     public function Add_new_article() {
         $add_name = htmlspecialchars($_POST['name'], ENT_QUOTES);
         $add_content = htmlspecialchars($_POST['content'], ENT_QUOTES);
-        return $this->sql_object->Add_new($this->sql_table->table_news, $add_name, $add_content);
+        return $this->sql_object->Add_new($this->sql_table, $add_name, $add_content);
     }
 
     public function Update_article() {
