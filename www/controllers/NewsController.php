d50b132f09acc4bbd3dd5137c4567b8fa4271bba
ryzhikk <dianakrylowa@yandex.ru>
2015-02-26 18:25:14 +0300

    Гм

--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -2,52 +2,44 @@
 
 class NewsController
 {
+    public $act;
+
+    public function __construct($actDir)
+    {
+        $this->act = $actDir;
+    }
+
     public function actionAll()
     {
         $add = $_COOKIE['add'];
         setcookie('add', '', time()-3600);
 
         $news = News::GetAllArticle();
-        require __DIR__ . '/../views/news/index.php';
 
+        $view = new View();
+        $view->data($news);
+        $view->display(News::$sql_table, $this->act);
     }
 
     public function actionOne()
     {
-        if (!empty ($_GET['id'])) {
+        if (!empty ($_GET['id']))
+        {
 
         $get_new = new News();
         $get_new->id = $_GET['id'];
-        $new = $get_new->GetOneArticle();
-        require __DIR__ . '/../views/news/new.php';
+        $news = $get_new->GetOneArticle();
+
+
+        $view = new View();
+        $view->data($news);
+        $view->display(News::$sql_table, $this->act);
+        }
+
+        else
+        {
+            return 'Error!';
         }
     }
 
-    public function actionNew()
-    {
-        require __DIR__ . '/../functions/session.php';
-
-        if (!empty ($_POST['send_new'])) {
-
-            $upload_news = new News();
-
-            if (News::AddNewArticle()) {
-                setcookie('add', 'new', time()+3600);
-                header ('Location: /index.php');
-            }
-
-            else {
-                echo 'Ошибка!';
-            }
-        }
-        require __DIR__ . '/../views/news/add_new.php';
-        var_dump($_SESSION);
-
-    }
-
-    public function actionEdit()
-    {
-        require __DIR__ . '/../views/news/edit_new.php';
-
-    }
 }
