3be100c661471758d9ac4f59e47e1355a180d69f
ryzhikk <dianakrylowa@yandex.ru>
2015-02-26 18:25:27 +0300

    Гм

--- www/classes/News.php
+++ www/classes/News.php
@@ -4,6 +4,6 @@ require __DIR__ . '/AbstractArticle.php';
 class News extends AbstractArticle {
 
     protected static $class = 'News';
-    protected static $sql_table = 'news';
+    public static $sql_table = 'news';
 
 }
