5281226bd0b8633db4dfa085d28a15927b29021e
ryzhikk <dianakrylowa@yandex.ru>
2015-03-18 21:44:28 +0300

    Exeptions

--- www/classes/Mailer.php
+++ www/classes/Mailer.php
@@ -10,7 +10,7 @@ class Mailer {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'dianakrylowa@gmail.com';
-        $mail->Password = 'lilkin17MAY2008dr';
+        $mail->Password = '';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
